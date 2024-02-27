368aca3df395075d2ab95fdd4457dc50b293ca41
aayushker <singhaayushker@gmail.com>
2024-02-28 01:40:46 +0530

    Added some JS for basic functioning

---
+++ script.js
@@ -0,0 +1,49 @@
+const b1 = document.getElementById('b1');
+const b2 = document.getElementById('b2');
+const b3 = document.getElementById('b3');
+const b4 = document.getElementById('b4');
+const b5 = document.getElementById('b5');
+const b6 = document.getElementById('b6');
+const b7 = document.getElementById('b7');
+const b8 = document.getElementById('b8');
+const b9 = document.getElementById('b9');
+
+const bp1 = document.getElementById('bp1');
+const bp2 = document.getElementById('bp2');
+const bp3 = document.getElementById('bp3');
+const bp4 = document.getElementById('bp4');
+const bp5 = document.getElementById('bp5');
+const bp6 = document.getElementById('bp6');
+const bp7 = document.getElementById('bp7');
+const bp8 = document.getElementById('bp8');
+const bp9 = document.getElementById('bp9');
+
+function main(){
+    b1.addEventListener('click', function(){
+        bp1.innerHTML = 'X';
+    });
+    b2.addEventListener('click', function(){
+        bp2.innerHTML = 'X';
+    });
+    b3.addEventListener('click', function(){
+        bp3.innerHTML = 'X';
+    });
+    b4.addEventListener('click', function(){
+        bp4.innerHTML = 'X';
+    });
+    b5.addEventListener('click', function(){
+        bp5.innerHTML = 'X';
+    });
+    b6.addEventListener('click', function(){
+        bp6.innerHTML = 'X';
+    });
+    b7.addEventListener('click', function(){
+        bp7.innerHTML = 'X';
+    });
+    b8.addEventListener('click', function(){
+        bp8.innerHTML = 'X';
+    });
+    b9.addEventListener('click', function(){
+        bp9.innerHTML = 'X';
+    });
+}
