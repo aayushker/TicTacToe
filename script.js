63aa88f8ce9023008e67110c9c04e46391c72088
aayushker <singhaayushker@gmail.com>
2024-02-28 12:24:31 +0530

    Made a basic prototype

--- script.js
+++ script.js
@@ -18,32 +18,72 @@ const bp7 = document.getElementById('bp7');
 const bp8 = document.getElementById('bp8');
 const bp9 = document.getElementById('bp9');
 
-function main(){
-    b1.addEventListener('click', function(){
-        bp1.innerHTML = 'X';
-    });
-    b2.addEventListener('click', function(){
-        bp2.innerHTML = 'X';
-    });
-    b3.addEventListener('click', function(){
-        bp3.innerHTML = 'X';
-    });
-    b4.addEventListener('click', function(){
-        bp4.innerHTML = 'X';
-    });
-    b5.addEventListener('click', function(){
-        bp5.innerHTML = 'X';
-    });
-    b6.addEventListener('click', function(){
-        bp6.innerHTML = 'X';
-    });
-    b7.addEventListener('click', function(){
-        bp7.innerHTML = 'X';
-    });
-    b8.addEventListener('click', function(){
-        bp8.innerHTML = 'X';
-    });
-    b9.addEventListener('click', function(){
-        bp9.innerHTML = 'X';
-    });
+let n = 1;
+
+function main(button){
+    const idButton = button.id;
+    if (n%2!=0){
+        button.innerHTML = "X";
+    }
+    else{
+        button.innerHTML = "O";
+    }
+    n = n+1;
+    check();
+}
+
+function reset(){
+    b1.innerHTML = "";
+    b2.innerHTML = "";
+    b3.innerHTML = "";
+    b4.innerHTML = "";
+    b5.innerHTML = "";
+    b6.innerHTML = "";
+    b7.innerHTML = "";
+    b8.innerHTML = "";
+    b9.innerHTML = "";
+    n = 1;
+}
+
+function check(){
+    if ((b1.innerHTML == "X" && b2.innerHTML == "X" && b3.innerHTML == "X") || (b1.innerHTML == "O" && b2.innerHTML == "O" && b3.innerHTML == "O")){
+        b1.style.backgroundColor = "green";
+        b2.style.backgroundColor = "green";
+        b3.style.backgroundColor = "green";
+    }
+    if ((b4.innerHTML == "X" && b5.innerHTML == "X" && b6.innerHTML == "X") || (b4.innerHTML == "O" && b5.innerHTML == "O" && b6.innerHTML == "O")){
+        b4.style.backgroundColor = "green";
+        b5.style.backgroundColor = "green";
+        b6.style.backgroundColor = "green";
+    }
+    if ((b7.innerHTML == "X" && b8.innerHTML == "X" && b9.innerHTML == "X") || (b7.innerHTML == "O" && b8.innerHTML == "O" && b9.innerHTML == "O")){
+        b7.style.backgroundColor = "green";
+        b8.style.backgroundColor = "green";
+        b9.style.backgroundColor = "green";
+    }
+    if ((b1.innerHTML == "X" && b4.innerHTML == "X" && b7.innerHTML == "X") || (b1.innerHTML == "O" && b4.innerHTML == "O" && b7.innerHTML == "O")){
+        b1.style.backgroundColor = "green";
+        b4.style.backgroundColor = "green";
+        b7.style.backgroundColor = "green";
+    }
+    if ((b2.innerHTML == "X" && b5.innerHTML == "X" && b8.innerHTML == "X") || (b2.innerHTML == "O" && b5.innerHTML == "O" && b8.innerHTML == "O")){
+        b2.style.backgroundColor = "green";
+        b5.style.backgroundColor = "green";
+        b8.style.backgroundColor = "green";
+    }
+    if ((b3.innerHTML == "X" && b6.innerHTML == "X" && b9.innerHTML == "X") || (b3.innerHTML == "O" && b6.innerHTML == "O" && b9.innerHTML == "O")){
+        b3.style.backgroundColor = "green";
+        b6.style.backgroundColor = "green";
+        b9.style.backgroundColor = "green";
+    }
+    if ((b1.innerHTML == "X" && b5.innerHTML == "X" && b9.innerHTML == "X") || (b1.innerHTML == "O" && b5.innerHTML == "O" && b9.innerHTML == "O")){ 
+        b1.style.backgroundColor = "green";
+        b5.style.backgroundColor = "green";
+        b9.style.backgroundColor = "green";
+    }
+    if ((b3.innerHTML == "X" && b5.innerHTML == "X" && b7.innerHTML == "X") || (b3.innerHTML == "O" && b5.innerHTML == "O" && b7.innerHTML == "O")){ 
+        b3.style.backgroundColor = "green";
+        b5.style.backgroundColor = "green";
+        b7.style.backgroundColor = "green";
+    }
 }
